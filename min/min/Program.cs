58be776a22bea5813b61edd9a4355a00ddcf43f9
Asgar9 <asgaryas@code.edu.az>
2022-08-03 11:26:24 +0400

    minimum value finder

---
+++ min/min/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] litt = new int[] {17 , 23, 15, 47, 7, 49 };
+            int min = litt[0];
+            for (int i = 0; i < litt.Length; i++)
+            {
+                if (min<=litt[i])
+                {
+
+                }
+                else
+                {
+                    min = litt[i];
+                }
+            }
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+    }
+}
